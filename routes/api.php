629d7fa2e251c6dc33868c82ded6740faa23032d
anamboijohn <anamboijohn@gmail.com>
2024-08-10 20:45:00 +0000

    wrap inside auth middle ware

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,18 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+
+
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware(['auth:sanctum'])->post('/logout', [AuthController::class, 'logout']);
+
+Route::get('/test', function () {
+    return response([
+        'name' => "John Anamboi",
+        "happy" => true,
+        "friend" => "only Christopher"
+    ]);
 });
