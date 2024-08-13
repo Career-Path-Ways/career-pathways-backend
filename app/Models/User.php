611d51a4a514022579edeeca7d04ac94def6d483
anamboijohn <anamboijohn@gmail.com>
2024-08-13 11:58:04 +0000

    define relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -39,5 +40,10 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function interests(): BelongsToMany
+    {
+        return $this->belongsToMany(AvailableJob::class, 'available_job_users', 'user_id', 'job_id')->withTimestamps()->withPivot('id');
     }
 }
