970318f5fef4cb378d76fb918c5b0936716fa883
anamboijohn <anamboijohn@gmail.com>
2024-08-13 11:59:27 +0000

    add more fields to factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,13 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'first_name' => fake()->name(),
+            'last_name' => fake()->name(),
+            'username' => fake()->userName(),
+            'password' => bcrypt('password'),
+            'date_of_birth' => '1990-01-01',
+            'email' => fake()->unique()->email(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
         ];
     }
 
@@ -37,7 +39,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }
