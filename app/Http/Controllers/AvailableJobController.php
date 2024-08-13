d00c8c049798c133f4202a7ee438fdca68b17c6c
anamboijohn <anamboijohn@gmail.com>
2024-08-13 11:57:49 +0000

    define relationship

--- app/Http/Controllers/AvailableJobController.php
+++ app/Http/Controllers/AvailableJobController.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\AvailableJob;
 use Illuminate\Http\Request;
 
 class AvailableJobController extends Controller
 {
-    //
+    public function index()
+    {
+        $jobs = AvailableJob::all();
+        return response([
+            'jobs' => $jobs
+        ]);
+    }
 }
--- app/Models/AvailableJob.php
+++ app/Models/AvailableJob.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class AvailableJob extends Model
 {
     use HasFactory;
+    protected $guarded = [];
+
+    protected $with = ['company'];
+
+    public function applicants(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'available_job_users', 'job_id', 'user_id')->withTimestamps()->withPivot('id');
+    }
+
+    public function company(): BelongsTo
+    {
+        return $this->belongsTo(company::class);
+    }
 }
