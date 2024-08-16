7c0887cf84e7898546335e1cfb3b55977782b55d
anamboijohn <anamboijohn@gmail.com>
2024-08-16 14:48:29 +0000

    store companies

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\company as CompanyModel;
+use Faker\Provider\ar_EG\Company;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $attributes = $request->validate([
+            'name' => ['string', 'required'],
+            'logo' => ['required', 'image', 'max:2048'],
+            'location' => ['string', 'required'],
+            'phone' => ['string', 'required'],
+        ]);
+
+        $logo = $this->saveImage($request->logo, 'CompanyLogo');
+        $attributes['logo'] = $logo;
+        $company = CompanyModel::create($attributes);
+
+        return response([
+            'company' => $company,
+            'message' => 'Company creaated successfully'
+        ], 200);
+    }
 }
